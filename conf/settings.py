f9ae7cf91cf5e1bb72f7a544acd1d18ce7781767
Nozimjon Hamdamov <mac@MacBook-Pro-M2-2022--Nozimjon.local>
2025-02-15 23:04:49 +0500

    deployment;

--- conf/settings.py
+++ conf/settings.py
@@ -129,7 +129,7 @@ LOCALE_PATHS = [BASE_DIR / 'locale']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR / 'static/')]
 
